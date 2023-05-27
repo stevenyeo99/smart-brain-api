fd39d1c42f465efa5ec4826a617deb8411a9de70
steven.yeo <steven.y@dynrtech.com>
2023-05-27 23:43:55 +0700

    distinct node script

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.post('/imageurl', (req, res) => {
     handleApiCall(req, res);
 })
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
